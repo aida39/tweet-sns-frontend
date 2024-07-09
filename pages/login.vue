f25d9284235af98ec8028807a77e42940022a2d5
aida <alma2013mtk@gmail.com>
2024-07-09 09:42:48 +0900

    pages/register.vue,pages/login.vue作成

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="container">
         <AuthHeader></AuthHeader>
+        <div class="auth-form">
+            <form action="">
+                <h1 class="auth-title">ログイン</h1>
+                <input class="auth-input" type="email" name="email" placeholder="メールアドレス">
+                <input class="auth-input" type="password" placeholder="パスワード">
+                <button class="auth-button">ログイン</button>
+            </form>
+        </div>
     </div>
 </template>
-
-<style scoped></style>
--- pages/register.vue
+++ pages/register.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="container">
         <AuthHeader></AuthHeader>
+        <div class="auth-form">
+            <form action="">
+                <h1 class="auth-title">新規登録</h1>
+                    <input class="auth-input" type="text" name="name" placeholder="ユーザーネーム">
+                    <input class="auth-input" type="email" name="email" placeholder="メールアドレス">
+                    <input class="auth-input" type="password" placeholder="パスワード">
+                <button class="auth-button">新規登録</button>
+            </form>
+        </div>
     </div>
 </template>
-
-<style scoped>
-
-</style>
